1bd2c407fcec7e8e932992765e0e598ce33bb3bd
sugshahe <sugra.shaheen@publicissapient.com>
2023-11-07 13:00:09 +1100

    new css changes

--- blocks/testblock/testblock.js
+++ blocks/testblock/testblock.js
@@ -4,14 +4,14 @@ export default function decorate(block) {
   /* decorate the testblock*/
   const testBlock= block.querySelector(':scope > div > div');
   const blockTestBlock= document.createElement('testblock');
-  blockTestBlock.innerHTML= `"${testBlock.innerHTML}"`;
+  blockTestBlock.innerHTML= '"${testBlock.innerHTML}"';
   testBlock.parentElement.replaceWith(blockTestBlock);
 
   /* decorate the author*/
   const authortestBlock= block.querySelector(':scope > div > div');
   if(authortestBlock){
   const p= document.createElement('p');
-  p.innerHTML= `<em> - ${authortestBlock.innerHTML} </em>`;
+  p.innerHTML= '<em> - ${authortestBlock.innerHTML} </em>';
   authortestBlock.parentElement.replaceWith(p);
 }
 }
